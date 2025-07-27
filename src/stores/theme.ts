09d564c5289bc3ac14bc5d792a3c84b772e94c6a
nbensalah <nabil.ben-salah@talan.com>
2025-07-27 17:08:00 +0100

    update theme

--- src/stores/theme.ts
+++ src/stores/theme.ts
@@ -2,7 +2,7 @@ import { writable } from 'svelte/store';
 import themes from '../../themes.json';
 import type { Theme } from '../interfaces/theme';
 
-const defaultColorscheme: Theme = themes.find((t) => t.name === 'argonaut')!;
+const defaultColorscheme: Theme = themes.find((t) => t.name === 'Argonaut')!;
 
 export const theme = writable<Theme>(
   JSON.parse(
